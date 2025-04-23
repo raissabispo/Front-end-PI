eb6aaa96f264952618d7bbb5bee879087fd2d5b5
Raissa Vitória <123013855+raissabispo@users.noreply.github.com>
2025-04-23 12:16:57 +0000

    Atualiza o dist

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Front-end-PI/',  // Defina o subdiretório de hospedagem aqui
+  base: '/', // ou apenas remova a linha inteira
+// Defina o subdiretório de hospedagem aqui
 })
