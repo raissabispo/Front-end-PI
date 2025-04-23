c5e28dba7e25e0ecb1486645f5568369f1754989
Raissa Vitória <123013855+raissabispo@users.noreply.github.com>
2025-04-23 12:56:58 +0000

    Adiciona Service Worker e Manifest

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa'; // Importação nomeada correta
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // ou apenas remova a linha inteira
-// Defina o subdiretório de hospedagem aqui
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'SIOP',
+        short_name: 'SIOP',
+        description: 'Sistema De Identficação Odonto-Pericial',
+        theme_color: '#0a74da',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+  base: '/',
+});
