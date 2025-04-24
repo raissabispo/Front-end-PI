594536d7ede2b73681848377439497caa0093488
Raissa <uamr.ieadpe1@gmail.com>
2025-04-23 21:30:56 -0300

    Altera icones,  adiciona screenshots no manifest e finaliza o PWA!

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7jqheg28pho"
+    "revision": "0.pmjhoencub"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
